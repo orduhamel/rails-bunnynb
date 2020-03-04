4562fc848fb9fd73843aec15d2089cb6ca735570
Johanne GUEGUEN <johanne.gueguen@gmail.com>
2020-03-04 11:26:00 +0100

    création d'index, new et create

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,12 +1,27 @@
 class BookingsController < ApplicationController
   def index
-    @bookings = Booking.all
+    @bookings = current_user.bookings
     #@user = User.find(params[:id])
   end
 
-  # def create
-  #   @booking = Booking.new(booking_params)
-  #   @booking.user = @user
-  #   redirect_to bunny_bookings_path
-  # end
+  def new
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @booking.bunny = Bunny.find(params[:id])
+    @booking.user = current_user
+    if @booking.save
+      redirect_to bookings_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:total_price, :status, :start_date, :end_date)
+  end
 end
