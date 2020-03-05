322dfb56e8f77978fb079f912a078c0a0acf3d4e
Ophelie Raymond Duhamel <ophe.duhamel@gmail.com>
2020-03-05 17:51:33 +0100

    Editing Owner bookings index method and view

--- app/controllers/owner/bookings_controller.rb
+++ app/controllers/owner/bookings_controller.rb
@@ -1,7 +1,7 @@
 class Owner::BookingsController < ApplicationController
   def index
     # Afficher tous les bookings de lapins dont le current_user est le owner
+    @user = current_user
     @bunnies = current_user.bunnies
-    @bookings = @bunnies.bookings
   end
 end
