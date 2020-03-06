498cc567bb8e3469a32ab7a94baeeccc444c5c65
Johanne GUEGUEN <johanne.gueguen@gmail.com>
2020-03-06 13:56:54 +0100

    patch done

--- app/controllers/owner/bookings_controller.rb
+++ app/controllers/owner/bookings_controller.rb
@@ -4,4 +4,16 @@ class Owner::BookingsController < ApplicationController
     @user = current_user
     @bunnies = current_user.bunnies
   end
+
+  def confirm
+    @booking = Booking.find(params[:id])
+    @booking.update(status: "confirmed")
+    redirect_to owner_bookings_path
+  end
+
+  def refuse
+    @booking = Booking.find(params[:id])
+    @booking.update(status: "refused")
+    redirect_to owner_bookings_path
+  end
 end
