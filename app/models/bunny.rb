3a91512a7f01572571b073a59860b0af90bb43b7
Ophelie Raymond Duhamel <ophe.duhamel@gmail.com>
2020-03-06 13:05:37 +0100

    Implementation du formulaire de créa dun bunny - en attente de la trad des radio boutons

--- app/models/bunny.rb
+++ app/models/bunny.rb
@@ -5,11 +5,21 @@ class Bunny < ApplicationRecord
 
   validates :name, :description, :address, :price_per_day, presence: true
   validates :price_per_day, numericality: true
-  # validates :fur, inclusion: { in: %w(rough soft fluffy) }
-  # validates :size, inclusion: { in: %w(dwarf small medium large giant) }
-  # validates :weight, inclusion: { in: %w(light normal heavy enormous) }
-  # validates :skill, inclusion: { in: %w(cuddly breeding partygoer calming acrobat boxer) }
-  # validates :cleanliness, inclusion: { in: (0..5) }
+
+  FURS = %w(rough soft fluffy)
+  validates :fur, inclusion: { in: FURS }
+
+  SIZES = %w(dwarf small medium large giant)
+  validates :size, inclusion: { in: SIZES }
+
+  WEIGHTS = %w(light normal heavy enormous)
+  validates :weight, inclusion: { in: WEIGHTS }
+
+  SKILLS = %w(cuddly breeding partygoer calming acrobat boxer)
+  validates :skill, inclusion: { in: SKILLS }
+
+  validates :cleanliness, inclusion: { in: (0..5) }
+
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
 
